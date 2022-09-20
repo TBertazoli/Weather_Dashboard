1f7b1af3ad5e5b59df87783ac1dfb0188323e839
TB <tatianabertazoli@gmail.com>
2022-09-20 18:02:44 -0500

    added color to the containers and fixed get storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,11 @@ $(document).ready(() => {
 });
 
 //function to get the geocordinates
-var getCityLocation = function (cityName) {
-    console.log(cityName);
+var getCityLocation = function () {
+    //this if is for empty inputText
+    if (inputText.val() === "") {
+        return;
+    }
     var coordinatesApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + inputText.val() + "&limit=1" + "&appid=4b10fa1681c38a96d7bd9c68c024b6a4";
     fetch(coordinatesApi).then(function (response) {
         response.json().then(function (cities) {
@@ -26,10 +29,15 @@ var getWeatherReport = function (city) {
     var cityApi = "https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=" + city.lat + "&lon=" + city.lon + "&appid=4b10fa1681c38a96d7bd9c68c024b6a4";
     fetch(cityApi).then(function (response) {
         response.json().then(function (data) {
-            console.log(data);
             $("#cityname").text(data.name);
+            //check cityHistory index for repeating data            
+            if (cityHistory.indexOf(data.name) < 0) {
+                cityHistory.unshift(data.name);
+                if (cityHistory.length > 8) {
+                    cityHistory.splice(8, 1);
+                }
+            }
             //set local storage
-            cityHistory.push(data.name);
             localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
             displayCityHistory();
             $("#date").text(todaysDate);
@@ -39,7 +47,7 @@ var getWeatherReport = function (city) {
             $("#icon").attr("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
         });
     });
-};
+}
 
 //function to display five day forecast
 var displayFiveDayForecast = function (city) {
@@ -49,7 +57,7 @@ var displayFiveDayForecast = function (city) {
             $("#fivedayforecast").text("5-Day Forecast:");
             for (var i = 0; i < data.list.length; i++) {
                 var incrementDate = moment().add(i + 1, 'd').format("MM/DD/YYYY");
-                $('#day' + i).text(incrementDate);
+                $('#day' + i).text(incrementDate).css("font-weight", "bold");
                 $('#day' + i + 'temp').text("Temp: " + data.list[i].main.temp + "℉");
                 $('#day' + i + 'wind').text('Wind: ' + data.list[i].wind.speed + 'MPH');
                 $('#day' + i + 'humidity').text("Humidity: " + data.list[i].main.humidity + "%");
@@ -61,22 +69,18 @@ var displayFiveDayForecast = function (city) {
 
 var populateHistory = function () {
     cityHistory = JSON.parse(localStorage.getItem("cityHistory")) || [];
-    console.log(cityHistory);
     displayCityHistory();
 }
-
 
 var displayCityHistory = function () {
     $('#storecitysearch').empty();
     for (var i = 0; i < cityHistory.length; i++) {
-        console.log(cityHistory[i]);
-        
-        var createElement = $("<button type='submit' class='btn' onclick='searchFromHistory(\"" + cityHistory[i] +"\")'></button>").text(cityHistory[i]);
+        var createElement = $("<button type='submit' class='btn-history' onclick='searchFromHistory(\"" + cityHistory[i] + "\")'></button>").text(cityHistory[i]);
         $('#storecitysearch').append(createElement);
     }
 }
 
-var searchFromHistory = function(cityName) {
+var searchFromHistory = function (cityName) {
     inputText.val(cityName);
     getCityLocation();
 }
